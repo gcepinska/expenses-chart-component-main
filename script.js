a9152800daf50742474bc472feee58cbd86a9e3c
gcepinska <gcepinska@gmail.com>
2022-08-02 19:01:06 +0200

    bar chart added

--- script.js
+++ script.js
@@ -1,25 +1,79 @@
 "use strict";
 
-// fetch("./data.json")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((jsondata) => console.log(jsondata));
-
 const jsonData = function () {
   fetch("./data.json")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
+      const day = data.map((e) => e.day);
+      const amount = data.map((e) => e.amount);
+      // console.log(day);
+      // console.log(amount);
+      new Chart("myChart", {
+        type: "bar",
+        data: {
+          labels: day,
+          datasets: [
+            {
+              backgroundColor: "hsl(10, 79%, 65%)",
+              data: amount,
+              borderRadius: 5,
+              hoverBackgroundColor: "hsl(10, 100%, 85%)",
+              borderSkipped: "middle",
+            },
+          ],
+        },
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              backgroundColor: "hsl(25, 47%, 15%)",
+              caretSize: 0,
+              padding: 8,
+              caretPadding: 10,
+              yAlign: "bottom",
+              displayColors: false,
+              callbacks: {
+                title: (i) => {
+                  return ``;
+                },
+                label: function (context) {
+                  let label = context.dataset.label || "";
+
+                  if (label) {
+                    label += ": ";
+                  }
+                  if (context.parsed.y !== null) {
+                    label += new Intl.NumberFormat("en-US", {
+                      style: "currency",
+                      currency: "USD",
+                    }).format(context.parsed.y);
+                  }
+                  return label;
+                },
+              },
+            },
+          },
+          scales: {
+            x: {
+              grid: {
+                display: false,
+                drawBorder: false,
+              },
+            },
+            y: {
+              grid: {
+                display: false,
+                drawBorder: false,
+              },
+              ticks: { display: false },
+            },
+          },
+        },
+      });
     });
 };
 
 jsonData();
-
-
-
-const day = jsonData.day;
-const amount = jsonData.amount;
-
-
-
